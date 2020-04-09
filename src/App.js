2357e22d69a99222692d414ef6cf003536662e5e
conglt10 <conglt1712@gmail.com>
2020-04-09 11:34:08 +0700

    ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 import * as actions from './actions';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import LeaderBoard from './pages/Leaderboard';
+import Case from './pages/Case';
 
 function App() {
   const dispatch = useDispatch();
@@ -21,6 +23,8 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route path='/leaderboard' component={LeaderBoard} />
+          <Route path='/corona-case' component={Case} />
         </Switch>
       </BrowserRouter>
     </div>
