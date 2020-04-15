0409537f9b6050a7cb13e3bcfe837c157ff29bb1
conglt10 <conglt1712@gmail.com>
2020-04-15 14:34:14 +0700

    profile route

--- src/router.js
+++ src/router.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Home from './pages/Home';
 import LeaderBoard from './pages/Leaderboard';
 import Case from './pages/Case';
+import Profile from './pages/Profile';
 
 function Router() {
+  const threebox = useSelector((state) => state.threebox);
+  console.log(threebox);
   return (
     <Switch>
       <Route exact path='/' component={Home} />
       <Route path='/leaderboard' component={LeaderBoard} />
       <Route path='/corona-case' component={Case} />
+      <Route
+        path='/profile'
+        render={(props) => (
+          <Profile
+            {...props}
+            box={threebox.box}
+            space={threebox.space}
+            account={threebox.account}
+            threeBoxProfile={threebox.threeBoxProfile}
+          />
+        )}
+      />
     </Switch>
   );
 }
